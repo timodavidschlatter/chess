bc56a9351a19579c6d97dd846f06bec5262a3944
Timo David Schlatter <timo.schlatter@hotmail.com>
2020-04-30 17:57:07 +0200

    prototype of dynamically creating gridpane

--- Chess/src/main/java/controller/GameController.java
+++ Chess/src/main/java/controller/GameController.java
@@ -1,7 +1,37 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 
 public class GameController {
+
+    @FXML
+    private HBox main;
+
+    @FXML
+    private void initialize() {
+        createChessBoard();
+    }
+
+    /**
+     *
+     */
+    private void createChessBoard() {
+        GridPane chessBoard = new GridPane();
+        int a = 8;
+        for(int i = 0; i < a; i++) {
+            for(int j = 0; j < a; j++) {
+                //StackPane tile = new StackPane();
+                Button tile = new Button("Fuck You");
+                chessBoard.add(tile, i, j);
+            }
+        }
+        main.getChildren().add(chessBoard);
+    }
+
+
 
 }
--- Chess/src/main/java/module-info.java
+++ Chess/src/main/java/module-info.java
@@ -2,5 +2,6 @@ module Chess {
     requires javafx.graphics;
     requires javafx.fxml;
     requires javafx.controls;
-    opens start; //Need to open your javafx package bc of controls
+    opens start to javafx.fxml, javafx.graphics; //Need to open your javafx package bc of controls
+    opens controller to javafx.fxml;
 }
